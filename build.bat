fba6abda0c8b07f938395cbebabc0c9c6e4b26fb
Ian Bryant <ijb2020syr@gmail.com>
2023-05-12 20:10:41 -0400

    32 bit instead of 64

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 if not defined DevEnvDir (
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
 )
-cl /nologo /O1 /w /GS- main.c /Fe"snake.exe" /link /nodefaultlib /subsystem:windows kernel32.lib gdi32.lib user32.lib
+cl /arch:IA32 /nologo /O1 /w /GS- main.c /Fe"snake.exe" /link /nodefaultlib /subsystem:windows kernel32.lib gdi32.lib user32.lib
 rm main.obj
